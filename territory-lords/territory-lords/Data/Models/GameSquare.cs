0861e75aec0a487b6d9d05e6b4150703bd9aa03c
kevin lyman <lyman.kevin@gmail.com>
2021-02-10 19:07:55 -0600

    Game board object and game squares added. Also handles click of a gamesquare

---
+++ territory-lords/territory-lords/Data/Models/GameSquare.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace territory_lords.Data.Models
+{
+    public class GameSquare
+    {
+        public string Color { get; set; }
+    }
+}
