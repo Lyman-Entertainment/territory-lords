de0a62e287b13624462e869148f5e2d568d84d03
kevin lyman <lyman.kevin@gmail.com>
2021-02-28 13:27:48 -0600

    Getting some initial ocean and coastal displays set.

--- territory-lords/territory-lords/Data/Models/GameTile.cs
+++ territory-lords/territory-lords/Data/Models/GameTile.cs
@@ -7,6 +7,8 @@ namespace territory_lords.Data.Models
 {
     public class GameTile
     {
+        public int RowIndex { get; set; }
+        public int ColumnIndex { get; set; }
         public string Color { get; set; }
         public LandType LandType { get; set; }
         public string Improvement { get; set; }
