ca126f185369a245664ba9663fc3491c6104fd88
Kevin Lyman <lyman.kevin@gmail.com>
2023-04-22 11:48:44 -0500

    42 create city screen (#46)
    * Added in start for buildings, wonders, technologies
    
    * Added needed data and got static city screen to open and close
    
    * City screen is working and can get some city data
    
    * Some things got left out. Added them in

---
+++ territory-lords/territory-lords/Data/Technologies/SpaceFlight.cs
@@ -0,0 +1,13 @@
+﻿namespace territory_lords.Data.Technologies
+{
+    public class SpaceFlight : BaseTechnology
+    {
+        public SpaceFlight() : base()
+        {
+            Name = "Space Flight";
+            Price = 10;
+            RequiredTechs = new() {};//computers, Rocketry
+            TechnologyType = TechnologyTypes.SpaceFlight;
+        }
+    }
+}
